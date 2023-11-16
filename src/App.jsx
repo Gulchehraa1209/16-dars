1505ed3c8c7ad4b895e8a0c6e34b3f239a89cd9f
Gulchexra <gulchexra@gmail.com>
2023-11-16 17:36:38 +0500

    AaA

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,36 @@
 import React, { Component } from 'react'
-import { Switch, Route, Link } from "react-router-dom"
-import Product from './Product'
-import Cabinet from './Cabinet'
-import Settings from './Settings'
-import Dashboard from './Dashboard'
-import Balance from './Balance'
-import OneProduct from './OneProduct'
-import Details from './Details'
-import LastPage from './LastPage'
+import {Switch, Route, Link} from "react-router-dom"
+import OptionOne from './Component/OptionOne'
+import OptionTwo from './Component/OptionTwo'
+import OptionThree from './Component/OptionThree'
+import OptionFour from './Component/OptionFour'
+import OptionFive from './Component/OptionFive'
 
 export default class App extends Component {
   render() {
     return (
       <div className='container'>
-        <div className="row my-4">
-          <div className="col-md-12">
-            <Link to="/" className="btn btn-dark mx-2">H0ME</Link>
-            <Link to="/product" className="btn btn-success mx-2">Product</Link>
-            <Link to="/cabinet" className="btn btn-info mx-2">Cabinet</Link>
-          </div>
-          <br />
-          <br />
-          <hr />
-          <Switch>
-            <Route path="/product/:id/settings/details/:id" component={LastPage} />
-            <Route path="/product/:id/settings/details" component={Details} />
-            <Route path="/product/:id/settings" component={Settings} />
-            <Route path="/product/:id" component={OneProduct} />
-            <Route path="/cabinet/dashboard/balance" component={Balance} />
-            <Route path="/cabinet/dashboard" component={Dashboard} />
-            <Route path="/cabinet/settings" component={Settings} />
-            <Route path="/product" component={Product} />
-            <Route path="/cabinet" component={Cabinet} />
-          </Switch>
-        </div>
+                <div className="row my-4">
+                    <div className="col-md-6">
+                            <Link to="/"> <button className='btn btn-success'>HOME</button> </Link>
+                            <Link to="/optionOne"> <button className='btn btn-success'>Option1</button> </Link>
+                            <Link to="/optionTwo"> <button className='btn btn-success'>Option2</button> </Link>
+                            <Link to="/optionThree"> <button className='btn btn-success'>Option3</button> </Link>
+                            <Link to="/optionFour"> <button className='btn btn-success'>Option4</button> </Link>
+                            <Link to="/optionFive"> <button className='btn btn-success'>Option5</button> </Link>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="card card-body p-5">
+                            <Switch>
+                                <Route path="/optionOne" component={OptionOne}/>
+                                <Route path="/optionTwo" component={OptionTwo}/>
+                                <Route path="/optionThree" component={OptionThree}/>
+                                <Route path="/optionFour" component={OptionFour}/>
+                                <Route path="/optionFive" component={OptionFive}/>
+                            </Switch>
+                        </div>
+                    </div>
+                </div>
       </div>
     )
   }
